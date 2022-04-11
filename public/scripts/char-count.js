a4f20436a489eefae9332f2938266d9db7b00852
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-11 11:33:35 -0600

    Add login page

---
+++ public/scripts/char-count.js
@@ -0,0 +1,18 @@
+/* $(document).ready(function() {
+  const maxLength = 150;
+  $('char-count').text(maxLength)
+
+  $("textarea.story-text-input").on("input", function(){
+    const count = $('#char-count')
+    const input = $(this).val().length;
+    count.text(maxLength - input)
+
+    if (input > maxLength) {
+      count.addClass('red')
+    } else {
+      count.removeClass('red')
+    }
+
+  });
+});
+ */
