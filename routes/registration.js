b2e18a9701c797c9bb1efa03a05c3282e4f17be7
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-11 19:15:37 -0600

    template change

--- routes/registration.js
+++ routes/registration.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+
 const registrationRoutes = (db) => {
   router.get("/", (req, res) => {
     console.log("registration route")
     res.render("registration")
-  })
+    })
 
   return router;
 };
