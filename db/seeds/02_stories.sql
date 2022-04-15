fcee8716d6ab5b720a6ba075cdec1441d1e3de8a
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-15 10:59:02 -0600

    New stor seeds

--- db/seeds/02_stories.sql
+++ db/seeds/02_stories.sql
@@ -9,7 +9,17 @@ That rises to the sun
 And thats the answer, Heaven knows,
 Why chewing must be done...'),
 ('1', 'What happened', 'No one would have believed, in the last years of the nineteenth century, that this world was being watched keenly and closely by intelligences greater than mans and yet as mortal as his own; that as men busied themselves about their various concerns they were being scrutinized and studied, perhaps almost as narrowly as a man with a microscope might scrutinize the transient creatures that swarm and multiply in a drop of water.'),
-('1', 'Hello', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui culpa perferendis fugit expedita iure. Eligendi ipsa possimus quibusdam sit saepe reiciendis unde animi tempore, facere eos fugiat asperiores nihil cumque vel tempora voluptatem quia ullam provident. Cum in accusantium magni?'),
-('2', 'Hello', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident pariatur ullam architecto iusto, eos unde ipsum sit aspernatur numquam voluptas.'),
-('2', 'Hello', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident pariatur ullam architecto iusto, eos unde ipsum sit aspernatur numquam voluptas.'),
+('1', 'The Peculiar Banana', 'Jack Smart looked at the peculiar banana in his hands and felt ecstatic.
+
+He walked over to the window and reflected on his noisy surroundings. He had always loved hilly Falmouth with its blue, barbecued beaches. It was a place that encouraged his tendency to feel ecstatic.
+
+Then he saw something in the distance, or rather someone. It was the figure of Sophia Parker. Sophia was a friendly coward with red eyelashes and sticky elbows.'),
+('2', 'Admirable Roy Pitt', 'Maud Ferguson was thinking about Roy Pitt again. Roy was an admirable animal with handsome abs and wobbly eyelashes.
+
+Maud walked over to the window and reflected on her hilly surroundings. She had always loved snooty Truro with its blue-eyed, bored beaches. It was a place that encouraged her tendency to feel fuzzy.
+
+Then she saw something in the distance, or rather someone. It was the an admirable figure of Roy Pitt.'),
+('2', 'Optimistic Kate Butterscotch', 'Richard Blunder was thinking about Kate Butterscotch again. Kate was an optimistic coward with skinny abs and hairy arms.
+
+Richard walked over to the window and reflected on his grand surroundings. He had always loved creepy Camborne with its anxious, ancient arches. It was a place that encouraged his tendency to feel fuzzy.'),
 ('3', 'Hello', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident pariatur ullam architecto iusto, eos unde ipsum sit aspernatur numquam voluptas.');
